ab8fb4005cf4c6c4516fec6475cde454078edd30
choiyuyeon <cyy0067@naver.com>
2019-07-30 22:18:46 +0900

    crosshair added, move controller added

---
+++ Assets/Scripts/Character_Controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Controller : MonoBehaviour
+{
+    
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            anim.SetFloat("Vertical", 1f, 0.1f, Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+
+            anim.SetFloat("Vertical", -1f, 0.1f, Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+
+            anim.SetFloat("Horizontal", -1f, 0.1f, Time.deltaTime);
+        } 
+        else if(Input.GetKey(KeyCode.D))
+        {
+
+            anim.SetFloat("Horizontal", 1f, 0.1f, Time.deltaTime);
+        }
+        else
+        {
+            anim.SetFloat("Horizontal", 0f, 0.1f, Time.deltaTime);
+            anim.SetFloat("Vertical", 0f, 0.1f, Time.deltaTime);
+        }
+    }
+}
