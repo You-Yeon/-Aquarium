c22b077db0c4144f7a7511004686486c2ba98921
choiyuyeon <cyy0067@naver.com>
2019-08-01 19:59:51 +0900

    Blender Tree

--- Assets/Scripts/Character_Controller.cs
+++ Assets/Scripts/Character_Controller.cs
@@ -15,33 +15,64 @@ public class Character_Controller : MonoBehaviour
 
     void Update()
     {
+        Move();
+    }
 
-        if (Input.GetKey(KeyCode.W))
+    void Move ()
+    {
+
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A)) // ↖
         {
 
             anim.SetFloat("Vertical", 1f, 0.1f, Time.deltaTime);
+            anim.SetFloat("Horizontal", -0.5f, 0.1f, Time.deltaTime);
         }
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D)) // ↗
+        {
+            anim.SetFloat("Horizontal", 0.8f, 0.1f, Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A)) // ↙
+        {
 
-        else if (Input.GetKey(KeyCode.S))
+            anim.SetFloat("Vertical", -0.1f, 0.1f, Time.deltaTime);
+            anim.SetFloat("Horizontal", -1f, 0.1f, Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D)) // ↘
         {
 
             anim.SetFloat("Vertical", -1f, 0.1f, Time.deltaTime);
+            anim.SetFloat("Horizontal", -0.25f, 0.1f, Time.deltaTime);
         }
-
-        else if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.W)) // ↑
         {
 
+            anim.SetFloat("Vertical", 1f, 0.1f, Time.deltaTime);
+            anim.SetFloat("Horizontal", 0.5f, 0.1f, Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.S)) // ↓
+        {
+
+            anim.SetFloat("Vertical", -0.85f, 0.1f, Time.deltaTime);
             anim.SetFloat("Horizontal", -1f, 0.1f, Time.deltaTime);
-        } 
-        else if(Input.GetKey(KeyCode.D))
+        }
+
+        else if (Input.GetKey(KeyCode.A)) // ←
+        {
+            anim.SetFloat("Vertical", 0.66f, 0.1f, Time.deltaTime);
+            anim.SetFloat("Horizontal", -1f, 0.1f, Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D)) // →
         {
 
-            anim.SetFloat("Horizontal", 1f, 0.1f, Time.deltaTime);
+            anim.SetFloat("Vertical", -1f, 0.1f, Time.deltaTime);
+            anim.SetFloat("Horizontal", 0.66f, 0.1f, Time.deltaTime);
         }
-        else
+        else // *
         {
             anim.SetFloat("Horizontal", 0f, 0.1f, Time.deltaTime);
             anim.SetFloat("Vertical", 0f, 0.1f, Time.deltaTime);
         }
+
     }
 }
