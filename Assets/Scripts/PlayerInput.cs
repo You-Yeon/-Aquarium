9ac4b17aa47dd44f706e7e114ecbe1e1b298c6b8
choiyuyeon <cyy0067@naver.com>
2019-08-06 23:17:52 +0900

    mouse / fire

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -6,11 +6,15 @@ public class PlayerInput : MonoBehaviour
     public string moveAxisName = "Vertical"; // 앞뒤
     public string rotateAxisName = "Horizontal"; // 좌우 
     public string fireButtonName = "Fire1"; // 발사
+    public string mouseAxisNameX = "Mouse X"; // 마우스 X
+    public string mouseAxisNameY = "Mouse Y"; // 마우스 Y
 
     // 값 할당은 내부에서만 가능
     public float move { get; private set; } // 감지된 움직임 입력값
     public float rotate { get; private set; } // 감지된 회전 입력값
     public bool fire { get; private set; } // 감지된 발사 입력값
+    public float mouseX { get; private set; } // 감지된 마우스 입력값
+    public float mouseY { get; private set; } // 감지된 마우스 입력값
 
     // 매프레임 사용자 입력을 감지
     private void Update()
@@ -22,5 +26,9 @@ public class PlayerInput : MonoBehaviour
         rotate = Input.GetAxis(rotateAxisName);
         // fire에 관한 입력 감지
         fire = Input.GetButton(fireButtonName);
+        // mouseX에 관한 입력 감지
+        mouseX = Input.GetAxis(mouseAxisNameX);
+        // mouseY에 관한 입력 감지
+        mouseY = Input.GetAxis(mouseAxisNameY);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,15 @@
 
 // 플레이어 캐릭터를 사용자 입력에 따라 움직이는 스크립트
 public class PlayerMovement : MonoBehaviour {
-    public float moveSpeed = 0.1f; // 앞뒤 움직임의 속도
-    public float rotateSpeed = 80f; // 좌우 회전 속도
+    public float moveSpeed; // 앞뒤 움직임의 속도
+    public float mouseSpeed; // 마우스 속도
 
+    public float fireRate; // 발사 딜레이
+    public float fireTimer; // 타이머
 
+    public GameObject bulletPrefab; // 총알 Prefab
+    public GameObject shootPoint; // 발사 지점
+    
     private PlayerInput playerInput; // 플레이어 입력을 알려주는 컴포넌트
     private Rigidbody playerRigidbody; // 플레이어 캐릭터의 리지드바디
     private Animator playerAnimator; // 플레이어 캐릭터의 애니메이터
@@ -15,6 +20,12 @@ public class PlayerMovement : MonoBehaviour {
         playerInput = GetComponent<PlayerInput>();
         playerRigidbody = GetComponent<Rigidbody>();
         playerAnimator = GetComponent<Animator>();
+
+        moveSpeed = 5f; // 앞뒤 움직임의 속도
+        mouseSpeed = 2.0f; // 마우스 속도
+
+        fireRate = 0.3f; // 발사 딜레이
+        fireTimer = 0f; // 타이머
     }
 
     // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
@@ -26,9 +37,18 @@ public class PlayerMovement : MonoBehaviour {
         // 발사
         Fire();
 
+        // 마우스 회전
+        Mouse();
+
         // 입력 값에 따라 애니메이터의 Move 파라미터 값 변경
-        playerAnimator.SetFloat("Horizontal", playerInput.move);
-        playerAnimator.SetFloat("Vertical", playerInput.rotate);
+        playerAnimator.SetFloat("Vertical", playerInput.move);
+        playerAnimator.SetFloat("Horizontal", playerInput.rotate); 
+
+        if (fireTimer < fireRate)
+        {
+            fireTimer += Time.deltaTime;
+            Debug.Log(fireTimer);
+        }
     }
 
     // 입력값에 따라 캐릭터를 앞뒤로 움직임
@@ -46,9 +66,30 @@ public class PlayerMovement : MonoBehaviour {
 
     private void Fire()
     {
-        if (Input.GetButton("Fire1"))
+        if (fireTimer < fireRate) //마지막 발사 시간 간격이 fireRate보다 작으면 return
         {
-            playerAnimator.CrossFadeInFixedTime("Fire", 0.01f); // fire animation
+            return;
         }
+
+        if (playerInput.move != 0f || playerInput.rotate != 0f) // 움직이고 있는 상태면 return
+        {
+            return;
+        }
+
+        if (Input.GetButton("Fire1")) // 발사 버튼.
+        {
+
+            playerAnimator.CrossFadeInFixedTime("Fire", 0.01f); // fire animation
+
+            GameObject bullet = Instantiate(bulletPrefab, shootPoint.transform.position, shootPoint.transform.rotation);
+
+            fireTimer = 0.0f;
+        }
+    }
+
+    private void Mouse()
+    {
+        playerRigidbody.rotation = playerRigidbody.rotation * Quaternion.Euler(Vector3.up * mouseSpeed * playerInput.mouseX);
+
     }
 }
