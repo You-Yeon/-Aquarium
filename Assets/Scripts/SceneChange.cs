c6e54a2958235a170fc0dec757c271dacc64682c
choiyuyeon <cyy0067@naver.com>
2019-12-28 19:06:44 +0900

    intro canvas and character canvas

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class SceneChange : MonoBehaviour
 {
+    public GameObject Intro_canvas;
+    public GameObject Character_canvas;
+
     public void ChangeGameScene()
     {
         SceneManager.LoadScene("Main"); // 게임씬으로 이동
@@ -13,5 +16,11 @@ public class SceneChange : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();  // 게임 종료
+    }
+
+    public void SetCanvas()
+    {
+        Intro_canvas.SetActive(false); // 인트로 캔버스 off
+        Character_canvas.SetActive(true);  // 캐릭터 캔버스 on
     }
 }
